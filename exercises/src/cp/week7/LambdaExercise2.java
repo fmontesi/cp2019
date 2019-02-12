c5ce87ba69f9889ac4d47b18a67431241fa444d8
Fabrizio Montesi <famontesi@gmail.com>
2019-02-12 14:19:16 +0100

    Update LambdaExercise2.java

--- exercises/src/cp/week7/LambdaExercise2.java
+++ exercises/src/cp/week7/LambdaExercise2.java
@@ -7,6 +7,11 @@ package cp.week7;
 public class LambdaExercise2
 {
 	/*
+	NOTE: When I write Class::methodName, I don't mean to use a method reference (lambda expression), I'm simply
+	talking about a particular method.
+	*/
+	
+	/*
 	- Create a list of type ArrayList<String> with some elements of your preference.
 	- Create a Box that contains the list.
 	- Now sort the list using List::sort, by invoking Box::enter and by passing a lambda expression.
