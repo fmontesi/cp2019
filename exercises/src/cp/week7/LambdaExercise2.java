8e81c153077eff338b3b843af4033b178ae26731
Fabrizio Montesi <famontesi@gmail.com>
2019-02-12 14:18:08 +0100

    Update LambdaExercise2.java

--- exercises/src/cp/week7/LambdaExercise2.java
+++ exercises/src/cp/week7/LambdaExercise2.java
@@ -9,7 +9,7 @@ public class LambdaExercise2
 	/*
 	- Create a list of type ArrayList<String> with some elements of your preference.
 	- Create a Box that contains the list.
-	- Now sort the list using Arrays.sort, by invoking Box::enter and by passing a lambda expression.
+	- Now sort the list using List::sort, by invoking Box::enter and by passing a lambda expression.
 	  (You *cannot* use Box::content.)
 	*/
 }
